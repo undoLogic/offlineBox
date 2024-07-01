b40733db35749ddb40abcb365a98b50d98fcb857
Sacha-Lewis Dmytruk <lewis@undologic.com>
2024-07-01 00:15:06 -0400

    Auto create config file

--- sourceFiles/index.php
+++ sourceFiles/index.php
@@ -1,6 +1,42 @@
 <?php
 
-phpinfo();
+# Setup content
+$config = [
+    'urlsToDownload' => [
+        'www.offlincebox.com'
+    ],
+    'pathToSaveTo' => [
+        '/where/to/go'
+    ],
+    'urlToNotify' => [
+        'https://www.offlincebox.com/notifyEndPoint'
+    ]
+];
+$configFile = 'offlineBox_config.json';
+
+function pr($msg) {
+    echo '<pre>';
+    print_r($msg);
+    echo '</pre>';
+}
+function dd($msg) {
+    pr($msg);
+    exit;
+}
+
+if (!file_exists('offlineBox_config.json')) {
+    //pr('does not exist');
+    file_put_contents($configFile, json_encode($config, JSON_PRETTY_PRINT));
+    pr ('created config file');
+}
+
+
+
+$localConfig = file_get_contents($configFile);
+
+dd($localConfig);
+
+//phpinfo();
 
 //$offlineBox_shared_secret = '123';
 //
