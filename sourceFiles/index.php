cd3e9e679594eabd975a5ad94fba0539f1b5091d
Sacha-Lewis Dmytruk <lewis@undologic.com>
2024-07-01 11:46:23 -0400

    Added display of days since downloaded file

--- sourceFiles/index.php
+++ sourceFiles/index.php
@@ -10,5 +10,19 @@ $localConfig = json_decode($localConfig_json, true);
 
 foreach ($localConfig['pathsToSaveTo'] as $pathToSaveTo) {
     pr ('files at: '.$pathToSaveTo);
-    pr (listFile($pathToSaveTo));
+
+
+    foreach ( listFile($pathToSaveTo)  as $file) {
+
+        $timeOfDoiwnload = getLocalTime(convertFilenameToDateTime($file));
+
+        pr ('file: '.$timeOfDoiwnload.' - Days since: '.daysSinceDownload($timeOfDoiwnload));
+    }
 }
+
+
+//echo getLocalTime(date('Y-m-d H:i:s'));
+?>
+
+
+<a href="run.php">Manually run download</a>
--- sourceFiles/share.php
+++ sourceFiles/share.php
@@ -31,3 +31,37 @@ function listFile($dir) {
         $ignore
     );
 }
+
+
+function getLocalTime($utcTime, $format = 'Y-m-d H:i:s') {
+    $date = new DateTime($utcTime, new DateTimeZone('UTC'));
+    $date->setTimezone(new DateTimeZone('America/New_York'));
+    return $date->format($format);
+}
+
+function convertFilenameToDateTime($filename) {
+
+
+    //[2] => 2023-07-01_15-14-38.sql
+    $str = str_replace('.sql', '', $filename);
+
+    $str = str_replace("_", " ", $str);
+
+    $parts = explode(" ", $str);
+
+    $time = str_replace('-', ':', $parts[1]);
+
+    return $parts[0].' '.$time;
+
+
+}
+
+function daysSinceDownload($savedate) {
+    $savedatetime = strtotime($savedate);
+
+    $timeNow = strtotime('now');
+
+    $seconds = $savedatetime - $timeNow;
+
+    return round($seconds / (60 * 60 * 24));
+}
